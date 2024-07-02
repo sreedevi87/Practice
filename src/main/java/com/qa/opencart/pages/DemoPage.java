5e48d32e2931682bf133b6a1b73d95329378fda4
500apps-Sreedevi <79301160+500apps-Sreedevi@users.noreply.github.com>
2024-07-02 17:21:35 +0530

    added addtocart mtho dna d cart locator to demo page

--- src/main/java/com/qa/opencart/pages/DemoPage.java
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -1,5 +1,15 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class DemoPage {
+	By demoId=By.id("demo");
+	By Cart=By.xpath("cartxpath");
+	public void getDemo() {
+		System.out.println("inside demo page class");
+	}
+	public void addToCart() {
+		System.out.println("add to cart page");
+	}
 
 }
